fb8c3b7ef78f542e53fb0c29562e0e2d2d234c5f
egoelm <emilia.elm@hotmail.com>
2020-02-12 13:28:58 +0100

    added parentheses around print, removed building of model

--- build_corpus.py
+++ build_corpus.py
@@ -6,112 +6,122 @@ import lxml.etree as ET
 import os
 import regex
 
-# arguments setting 
+# arguments setting
 parser = argparse.ArgumentParser()
-parser.add_argument('--lcode', help='ISO 639-1 code of target language. See `lcodes.txt`.')
-parser.add_argument('--max_corpus_size', type=int, default=1000000000, help='the maximum size of the corpus. Feel free to adjust it according to your computing power.')
+parser.add_argument(
+    '--lcode', help='ISO 639-1 code of target language. See `lcodes.txt`.')
+parser.add_argument('--max_corpus_size', type=int, default=1000000000,
+                    help='the maximum size of the corpus. Feel free to adjust it according to your computing power.')
 args = parser.parse_args()
 
 lcode = args.lcode
 if lcode == 'ko':
-    from konlpy.tag import Kkma # pip install konlpy. See http://konlpy.org/en/v0.4.4/ for further information.
+    # pip install konlpy. See http://konlpy.org/en/v0.4.4/ for further information.
+    from konlpy.tag import Kkma
     kkma = Kkma()
-    print "kkma succesfuly loaded!"
+    print("kkma succesfuly loaded!")
 elif lcode == 'ja':
-    import MeCab # See https://pypi.python.org/pypi/mecab-python/0.996
+    import MeCab  # See https://pypi.python.org/pypi/mecab-python/0.996
     mecab = MeCab.Tagger("-Owakati")
-    print "mecab succesfuly loaded!"
+    print("mecab succesfuly loaded!")
 elif lcode == 'zh':
-    import jieba # See https://pypi.python.org/pypi/jieba/
-    print "jieba succesfuly loaded!"
+    import jieba  # See https://pypi.python.org/pypi/jieba/
+    print("jieba succesfuly loaded!")
 elif lcode == 'vi':
-    from pyvi.pyvi import ViTokenizer # See https://pypi.python.org/pypi/pyvi
-    print "pyvi succesfuly loaded!"
-elif lcode == 'th':  
-    import pythai # See https://pypi.python.org/pypi/pythai  
-    print "pythai succesfuly loaded!"
+    from pyvi.pyvi import ViTokenizer  # See https://pypi.python.org/pypi/pyvi
+    print("pyvi succesfuly loaded!")
+elif lcode == 'th':
+    import pythai  # See https://pypi.python.org/pypi/pythai
+    print("pythai succesfuly loaded!")
 # elif lcode == 'ar':
 #     os.environ['CLASSPATH'] = "../stanford-segmenter-2015-12-09"
 #     from nltk.tokenize.stanford_segmenter import StanfordSegmenter
-#     segmenter = StanfordSegmenter(path_to_jar="../stanford-segmenter-2015-12-09/stanford-segmenter-3.6.0.jar", 
-#                                path_to_sihan_corpora_dict="../stanford-segmenter-2015-12-09/data", 
-#                                path_to_model="../stanford-segmenter-2015-12-09/data/pku.gz", 
+#     segmenter = StanfordSegmenter(path_to_jar="../stanford-segmenter-2015-12-09/stanford-segmenter-3.6.0.jar",
+#                                path_to_sihan_corpora_dict="../stanford-segmenter-2015-12-09/data",
+#                                path_to_model="../stanford-segmenter-2015-12-09/data/pku.gz",
 #                                path_to_dict="../stanford-segmenter-2015-12-09/data/dict-chris6.ser.gz")
 #     print "StanfordSegmenter succesfuly loaded!"
-    
+
 max_corpus_size = args.max_corpus_size
-fname = "{}wiki-20161201-pages-articles-multistream.xml".format(lcode)    
+fname = "{}wiki-20200201-pages-articles-multistream.xml".format(lcode)
+
 
 def clean_text(text):
     global lcode
-    
+
     # Common
-    text = regex.sub("(?s)<ref>.+?</ref>", "", text) # remove reference links
-    text = regex.sub("(?s)<[^>]+>", "", text) # remove html tags
-    text = regex.sub("&[a-z]+;", "", text) # remove html entities
-    text = regex.sub("(?s){{.+?}}", "", text) # remove markup tags
-    text = regex.sub("(?s){.+?}", "", text) # remove markup tags
-    text = regex.sub("(?s)\[\[([^]]+\|)", "", text) # remove link target strings
-    text = regex.sub("(?s)\[\[([^]]+\:.+?]])", "", text) # remove media links
-    
-    text = regex.sub("[']{5}", "", text) # remove italic+bold symbols
-    text = regex.sub("[']{3}", "", text) # remove bold symbols
-    text = regex.sub("[']{2}", "", text) # remove italic symbols
-    
-    if lcode in ['ko']: # korean
-        text = regex.sub(u"[^ \r\n\p{Hangul}.?!]", " ", text) # Replace unacceptable characters with a space.
-    elif lcode in ['ja']: # japanese
-        text = regex.sub(u"[^\r\n\p{Han}\p{Hiragana}\p{Katakana}ー。！？]", "", text)
-    elif lcode in ['zh']: # chinsese
+    text = regex.sub("(?s)<ref>.+?</ref>", "", text)  # remove reference links
+    text = regex.sub("(?s)<[^>]+>", "", text)  # remove html tags
+    text = regex.sub("&[a-z]+;", "", text)  # remove html entities
+    text = regex.sub("(?s){{.+?}}", "", text)  # remove markup tags
+    text = regex.sub("(?s){.+?}", "", text)  # remove markup tags
+    # remove link target strings
+    text = regex.sub("(?s)\[\[([^]]+\|)", "", text)
+    text = regex.sub("(?s)\[\[([^]]+\:.+?]])", "", text)  # remove media links
+
+    text = regex.sub("[']{5}", "", text)  # remove italic+bold symbols
+    text = regex.sub("[']{3}", "", text)  # remove bold symbols
+    text = regex.sub("[']{2}", "", text)  # remove italic symbols
+
+    if lcode in ['ko']:  # korean
+        # Replace unacceptable characters with a space.
+        text = regex.sub(u"[^ \r\n\p{Hangul}.?!]", " ", text)
+    elif lcode in ['ja']:  # japanese
+        text = regex.sub(
+            u"[^\r\n\p{Han}\p{Hiragana}\p{Katakana}ー。！？]", "", text)
+    elif lcode in ['zh']:  # chinsese
         text = regex.sub(u"[^\r\n\p{Han}。！？]", "", text)
-    elif lcode in ['th']: # thai
+    elif lcode in ['th']:  # thai
         text = regex.sub(u"[^ \r\n\p{Thai}.?!]", " ", text)
-    elif lcode in ['ru']: # russian
+    elif lcode in ['ru']:  # russian
         text = regex.sub(u"[^ \r\n\p{Cyrillic}.?!\-]", " ", text)
         text = text.lower()
 #     elif lcode in ['ar']: # arabic
 #         text = regex.sub(u"[^ \r\n\p{Arabic}.?!\-]", " ", text)
-    elif lcode in ['hi']: # hindi
+    elif lcode in ['hi']:  # hindi
         text = regex.sub(u"[^ \r\n\p{Devanagari}.।?!\-]", " ", text)
-    elif lcode in ['bn']: # bengali
+    elif lcode in ['bn']:  # bengali
         text = regex.sub(u"[^ \r\n\p{Bengali}.।?!\-]", " ", text)
-    elif lcode in ['de']: # german
+    elif lcode in ['de']:  # german
         text = regex.sub(u"[^ \r\n\p{Latin}\-'‘’.?!]", " ", text)
-    else: # Mostly european languages
+    else:  # Mostly european languages
         text = regex.sub(u"[^ \r\n\p{Latin}\-'‘’.?!]", " ", text)
         text = text.lower()
-    
+
     # Common
-    text = regex.sub("[ ]{2,}", " ", text) # Squeeze spaces.
+    text = regex.sub("[ ]{2,}", " ", text)  # Squeeze spaces.
     return text
+
 
 def sentence_segment(text):
     '''
     Args:
       text: A string. A unsegmented paragraph.
-    
+
     Returns:
       A list of sentences.
     '''
     global lcode
     if lcode in ['ja', 'zh']:
-        sents = regex.split(u"([。！？])?[\n]+|[。！？]", text) 
+        sents = regex.split(u"([。！？])?[\n]+|[。！？]", text)
     elif lcode in ['th']:
-        sents = text.split("[\n]+") 
-    elif lcode in ['hi', 'bn']: # hindi, bengali
+        sents = text.split("[\n]+")
+    elif lcode in ['hi', 'bn']:  # hindi, bengali
         sents = regex.split(u"([.।?!])?[\n]+|[.।?!] ", text)
-    elif lcode in ['de']: # german
+    elif lcode in ['de']:  # german
         sents = regex.split("([.?!])?[\n]+|[.?!] ", text)
-        sents = [sent[0].lower() + sent[1:] for sent in sents if sent is not None and len(sent) > 1]
+        sents = [sent[0].lower() + sent[1:]
+                 for sent in sents if sent is not None and len(sent) > 1]
     else:
         sents = regex.split("([.?!])?[\n]+|[.?!] ", text)
     return sents
-        
+
+
 def word_segment(sent):
     '''
     Args:
       sent: A string. A sentence.
-    
+
     Returns:
       A list of words.
     '''
@@ -119,26 +129,27 @@ def word_segment(sent):
     if lcode in ['ko']:
         words = [word for word, _ in kkma.pos(sent)]
     elif lcode in ['ja']:
-        words = mecab.parse(sent.encode('utf8')).split() 
+        words = mecab.parse(sent.encode('utf8')).split()
     elif lcode in ['th']:
         words = pythai.split(sent)
     elif lcode in ['vi']:
-        words = ViTokenizer.tokenize(sent).split()        
+        words = ViTokenizer.tokenize(sent).split()
     elif lcode in ['zh']:
-        words = list(jieba.cut(sent, cut_all=False)) 
+        words = list(jieba.cut(sent, cut_all=False))
 #     elif lcode in ['ar']:
 #         words = segmenter.segment(sent).split()
-    else: # Mostly european languages
+    else:  # Mostly european languages
         words = sent.split()
-    
+
     return words
+
 
 def build_corpus():
     global lcode, max_corpus_size, fname
     with codecs.open("data/{}.txt".format(lcode), 'w', 'utf-8') as fout:
         i = 1
         j = 1
-        ns = "{http://www.mediawiki.org/xml/export-0.10/}" # namespace
+        ns = "{http://www.mediawiki.org/xml/export-0.10/}"  # namespace
         for _, elem in ET.iterparse("data/{}".format(fname), tag=ns+"text"):
             running_text = elem.text
             try:
@@ -149,21 +160,23 @@ def build_corpus():
                         words = word_segment(sent)
                         if len(words) > 10:
                             if lcode in ['ja']:
-                                fout.write(" ".join(words).decode('utf8') + "\n")
+                                fout.write(
+                                    " ".join(words).decode('utf8') + "\n")
                             else:
                                 fout.write(" ".join(words) + "\n")
-                                
+
             except:
-                continue # it's okay as we have a pretty big corpus!
-            elem.clear() # We need to save memory!
-            if i % 1000 == 0: 
-                print i,
+                continue  # it's okay as we have a pretty big corpus!
+            elem.clear()  # We need to save memory!
+            if i % 1000 == 0:
+                print(i),
                 fsize = os.path.getsize("data/{}.txt".format(lcode))
                 if fsize > max_corpus_size:
                     break
             i += 1
 
+
 if __name__ == "__main__":
     build_corpus()
-    
-    print "Done"
+
+    print("Done")
